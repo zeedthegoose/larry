d8de2ff01cd94b0411226e6a8252ed91c477ce8e
zeedthegoose <89756664+zeedthegoose@users.noreply.github.com>
2025-08-29 17:39:28 -0700

    trying some stuff

--- tests/test.h
+++ tests/test.h
@@ -12,8 +12,12 @@ typedef struct {
     test_func_t func;
     const char *name;
 } test_case_t;
+typedef struct {
+    char *name;
+    test_case_t tests[];
+} test_suite_t;
 
-extern test_case_t* TEST_SUITES[];
+extern test_suite_t TEST_SUITES[];
 
 void record_failure(const char* file, int line, const char* condition_str);
 
@@ -96,32 +100,39 @@ int main() {
     printf(C_B_WHITE "\nrunning tests\n\n" C_RESET);
 
     // run each suite
-    for (int i = 0; TEST_SUITES[i] != NULL; i++) {
-        test_case_t* current_suite = TEST_SUITES[i];
+    for (int i = 0; &TEST_SUITES[i] != NULL; i++) {
+        test_suite_t *current_suite = &TEST_SUITES[i];
 
-        for (int j = 0; current_suite[j].name != NULL; j++) {
+        for (int j = 0; current_suite->tests[j].name != NULL; j++) {
             printf("%-*s", 60, current_suite[j].name);
 
             int failures_before_test = test_fail_count;
-            current_suite[j].func();
+            current_suite->tests[j].func();
             int num_failures = test_fail_count - failures_before_test;
 
             if (num_failures > 0) {
-                printf("[ \033[1;31mfail\033[0m ]\n");
+                printf("[ " C_B_RED "fail" C_RESET " ]\n");
             } else {
-                printf("[ \033[1;32mok\033[0m ]\n");
+                printf("[ " C_B_GREEN "ok" C_RESET " ]\n");
             }
 
-            // print failures
-            while (failure_log->next != NULL) {
-                printf("  %s:%d: %s ... " C_B_RED "fail\n" C_RESET, failure_log->next->record->file, failure_log->next->record->line, failure_log->next->record->condition_str);
-                failure_log->next = failure_log->next->next;
-            }
         }
     }
 
-    printf("\n");
-    printf(C_B_WHITE "test results: " C_RESET);
+    // print failures
+    if (test_fail_count > 0) {
+        printf(C_B_RED "\nfailed assertions\n" C_RESET);
+    }
+    while (failure_log->next != NULL) {
+        printf("  %s:%d: %s\n", failure_log->next->record->file, failure_log->next->record->line, failure_log->next->record->condition_str);
+
+        failure_list_node_t *temp = failure_log->next;
+        failure_log->next = failure_log->next->next;
+        free(temp->record);
+        free(temp);
+    }
+
+    printf(C_B_WHITE "\ntest results: " C_RESET);
 
     if (test_fail_count > 0) {
         printf(C_B_RED "fail" C_RESET);
--- tests/test_runner.c
+++ tests/test_runner.c
@@ -2,9 +2,9 @@
 #include "test.h"
 
 // Bring in the lists of tests from other files
-extern test_case_t evaluator_tests[];
+extern test_suite_t evaluator_tests[];
 
-test_case_t* TEST_SUITES[] = {
-    evaluator_tests,
-    NULL
+test_suite_t TEST_SUITES[] = {
+    {"evaluator test", evaluator_tests},
+    {NULL},
 };
